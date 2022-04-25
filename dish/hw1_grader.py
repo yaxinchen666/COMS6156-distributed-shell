a22ad49f8f3d1587d8f4d184438e4bd345bd14a3
aidai beishekeeva <aidai.beishekeeva@gmail.com>
2022-04-25 18:17:16 -0400

    Grading script and config file for DISH shell

---
+++ dish/hw1_grader.py
@@ -0,0 +1,21 @@
+import unittest
+from calculator import * 
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(addition(1,3), 4, "Should be 4")
+
+    def test_sum_tuple(self):
+        self.assertEqual(addition(3, 4), 7, "Should be 6")
+
+class TestSubstraction(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(substraction(4,9), -5, "Should be -4")
+
+    def test_sum_tuple(self):
+        self.assertEqual(addition(9,0), 9, "Should be 9")
+
+if __name__ == '__main__':
+    unittest.main()
